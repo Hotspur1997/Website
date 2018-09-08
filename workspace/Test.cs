0ee0042ba0b2c7892a347a8c053fad7e66a01597
Marc Phua <hotspur1997@Marcs-MacBook-Pro.local>
2018-09-08 13:27:45 +0800

    first commit

---
+++ workspace/Test.cs
@@ -0,0 +1,14 @@
+using System;
+
+
+namespace Test1
+{
+    class myClass 
+    {
+        static void Main(string[] args)
+        {
+        double num = 3/2;
+        Console.WriteLine(num);
+        }  
+    }
+}
